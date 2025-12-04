1afaaba54325fd5a2356887019b7895cbf0e6c8f
swatantra19 <swatantrarajsingh1901@gmail.com>
2025-12-04 10:41:17 +0530

    more

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,26 +1,23 @@
 import { useState ,useEffect } from "react";
 
-     export function useLocalStorage( key,initiaValue){
+  export function useLocalStorage( key,initiaValue){
 
-          // 1. Initialize state
-  const [value, setValue] = useState(() => {
-    // Get stored value from local storage
+        
+    const [value, setValue] = useState(() => {
+    
     const jsonValue = localStorage.getItem(key);
     
-    // If we found a value, parse it and return it
     if (jsonValue != null) return JSON.parse(jsonValue);
     
-    // Otherwise, return the initial value
     return initiaValue;
-  });
+    });
 
 
-
-  // 2. Save state to local storage when it changes
-useEffect(() => {
-  localStorage.setItem(key, JSON.stringify(value));
-}, [key, value]);
+    useEffect(() => {
+     localStorage.setItem(key, JSON.stringify(value));}
+     , [key, value]);
 
 
-return [value, setValue];
-     }
+    return [value, setValue];
+
+  }
